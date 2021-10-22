5b0e70ab58de4a7a1fc422891254ce71f36a37df
Aasutosh <soniaasu2101@gmail.com>
2021-10-22 22:24:03 +0530

    collect form data

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,22 @@
-function clickHello() {
-	alert("Hello !!! " + document.getElementById('fullName').value);
+
+
+function clickHello(event) {
+	event.preventDefault();
+	var data={
+			fullName:'',
+			emailId:'',
+			phoneNumber:'',
+			studentId:'',
+			dob:'',
+			entryYear:''
+	}
+	
+	Object.keys(data).forEach(k=>{
+		data[k]=document.getElementById(k).value;
+	})
+	
+	data['gender']=document.getElementById('male').checked ? 'Male' : 'Female'
+	
+	console.log(data)
 }
 
