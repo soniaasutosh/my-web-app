6eaf0aaada572c8e6f3f3b267fed1f243d285229
Aasutosh <soniaasu2101@gmail.com>
2021-10-23 09:13:30 +0530

    collect html form value

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,21 +2,49 @@
 
 function clickHello(event) {
 	event.preventDefault();
-	var data={
-			fullName:'',
-			emailId:'',
-			phoneNumber:'',
-			studentId:'',
-			dob:'',
-			entryYear:''
-	}
-	
-	Object.keys(data).forEach(k=>{
-		data[k]=document.getElementById(k).value;
-	})
-	
-	data['gender']=document.getElementById('male').checked ? 'Male' : 'Female'
+	//const data = manual()
+	const data = autoGenerate();
 	
 	console.log(data)
+}
+
+function autoGenerate(){
+	var data={}
+	
+	const arr = [...document.getElementsByTagName('input'),...document.getElementsByTagName('select')];
+
+	arr.forEach(el=>{
+		if(el.type=='radio' && el.checked){
+			data[el.name]=el.id
+		}else if(el.type !='radio'){
+			data[el.name]=el.value;
+		}
+	})
+
+
+	return data;
+}
+
+function manual() {
+	var data = {
+		fullName: '',
+		emailId: '',
+		phoneNumber: '',
+		studentId: '',
+		dob: '',
+		entryYear: ''
+	}
+
+	// var keys = Object.keys(data);
+	// for (var i = 0; i < keys.length; i++) {
+	// 	var k = keys[i];
+	// 	data[k] = document.getElementById(k).value;
+	// }
+
+	Object.keys(data).forEach(k => data[k] = document.getElementById(k).value);
+
+	data['gender'] = document.getElementById('male').checked ? 'Male' : 'Female';
+
+	return data;
 }
 
